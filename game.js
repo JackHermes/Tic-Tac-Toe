0651e68b9c1ba45ce8f218734d7d47781cdbc2df
Jordan Scholten <schojo13@gmail.com>
2017-01-26 11:19:41 -0800

    Display board with unique rows

--- game.js
+++ game.js
@@ -4,15 +4,26 @@ var program = require('commander');
 
 program
   .version('0.0.1')
-  .usage('<keywords>')
-  // .command('add <number>', 'Add a number')
+  .usage('[options] <words>')
+  .option('-a, --add', 'Add')
+  .option('-b, --build', 'Build')
   // .action(function(number) {
   //   console.log(number);
   // })
   .parse(process.argv);
 
-if(!program.args.length) {
-  program.help();
-} else {
-  console.log('Keywords: ' + program.args);
-}
+  var words = program.args;
+  var row1 = ['_','_','_'];
+  var row2 = ['_','_','_'];
+  var row3 = ['_','_','_'];
+
+
+  if(program.add){
+    console.log('Adding');
+  }
+  if(program.build){
+    console.log('Building');
+    console.log(row1);
+    console.log(row2);
+    console.log(row3);
+  }
