1427f71c00c13360f9bf07cc2aa49bdfc22006c9
Jordan Scholten <schojo13@gmail.com>
2017-01-26 10:53:28 -0800

    Executable command 'game'

--- play.js
+++ play.js
@@ -1,1 +1,13 @@
 var program = require('commander');
+
+program
+  .version('0.0.1')
+  .usage('[options] <number>')
+  .command('add <number>', 'Add a number')
+  .action(function(number) {
+    console.log(number);
+  })
+  .parse(process.argv);
+
+console.log('hey');
+if(program.add) console.log(' added a piece');
