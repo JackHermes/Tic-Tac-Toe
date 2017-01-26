f7c033cc2b620594679136d7f99a2603b2d42e09
Jordan Scholten <schojo13@gmail.com>
2017-01-26 11:30:16 -0800

    Board with coordinates and can clear board

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var program = require('commander');
 program
   .version('0.0.1')
   .usage('[options] <words>')
-  .option('-a, --add', 'Add')
+  .option('-a, --add <n>', 'Add location')
   .option('-b, --build', 'Build')
   // .action(function(number) {
   //   console.log(number);
@@ -19,11 +19,20 @@ program
 
 
   if(program.add){
-    console.log('Adding');
+    console.log('Adding piece to ', program.add);
+    console.log([0],['A','B','C']);
+    console.log([1], row1);
+    console.log([2],row2);
+    console.log([3],row3);
   }
   if(program.build){
-    console.log('Building');
-    console.log(row1);
-    console.log(row2);
-    console.log(row3);
+    console.log('Building board', program.build);
+    row1 = ['_','_','_'];
+    row2 = ['_','_','_'];
+    row3 = ['_','_','_'];
+    
+    console.log([0],['A','B','C']);
+    console.log([1], row1);
+    console.log([2],row2);
+    console.log([3],row3);
   }
